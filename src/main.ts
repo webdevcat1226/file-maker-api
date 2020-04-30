a071afcb4d385b6cebd8d134929a5aae943e3738
Silver-it <silver.19920220@gmail.com>
2020-04-30 00:33:48 -0500

    feat: configure default port for heroku

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
 
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 
 bootstrap();
